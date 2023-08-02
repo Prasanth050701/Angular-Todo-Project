3e684d0d77cae9e72c4d065e2bf70241fc4e0d9e
Prasanth050701 <axechandru@gmail.com>
2023-08-02 17:32:22 +0530

    git commit -m "To-do"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Task {
+  description: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+    tasks: Task[] = [
+    { description: 'Task 1', completed: false },
+    { description: 'Task 2', completed: false },
+    { description: 'Task 3', completed: true },
+    { description: 'Task 4', completed: false }
+  ];
+
+  get totalTasks(): number {
+    return this.tasks.length;
+  }
+
+  get completedTasks(): number {
+    return this.tasks.filter(task => task.completed).length;
+  }
+
+  get pendingTasks(): number {
+    return this.totalTasks - this.completedTasks;
+  }
+  
+  toggleTaskCompletion(task: Task): void {
+    task.completed = !task.completed;
+  }
 }
